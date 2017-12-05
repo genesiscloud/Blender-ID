df20c7e37ab8c5f62b65138ff9152a6fedcf6769
Sybren A. Stüvel <sybren@stuvel.eu>
2017-12-05 12:44:19 +0100

    Don't call webhooks when fixing public_roles_as_string
    It takes far too long to update all users that way, and it's not necessary.

--- bid_main/management/commands/fix_roles_as_string.py
+++ bid_main/management/commands/fix_roles_as_string.py
@@ -10,6 +10,11 @@ class Command(BaseCommand):
     help = "Updates users' public_roles_as_string property based on their roles"
 
     def handle(self, *args, **options):
+        from bid_api import signals
+        from django.db.models.signals import post_save
+
+        post_save.disconnect(signals.modified_user_to_webhooks)
+
         self.stdout.write('Updating all users.')
 
         model: models.User = get_user_model()
@@ -28,4 +33,4 @@ class Command(BaseCommand):
             user.public_roles_as_string = ' '.join(sorted(roles))
             user.save(update_fields={'public_roles_as_string'})
 
-        self.stdout.write(self.style.SUCCESS('Done, updated {user_count} users.'))
+        self.stdout.write(self.style.SUCCESS(f'Done, updated {user_count} users.'))
