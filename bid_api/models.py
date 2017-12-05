6e449efce9490dd10ff76e97775fae09f2ed2135
Sybren A. Stüvel <sybren@stuvel.eu>
2017-12-05 12:13:06 +0100

    Generate random secret for new webhooks

--- bid_api/models.py
+++ bid_api/models.py
@@ -5,13 +5,19 @@ WEBHOOK_TYPES = [
 ]
 
 
+def create_random_secret() -> str:
+    import secrets
+
+    return secrets.token_urlsafe(32)
+
+
 class Webhook(models.Model):
     name = models.CharField(max_length=128)
     enabled = models.BooleanField(default=True)
     hook_type = models.CharField(max_length=32, choices=WEBHOOK_TYPES, default=WEBHOOK_TYPES[0][0],
                                  db_index=True)
     url = models.URLField()
-    secret = models.CharField(max_length=64)
+    secret = models.CharField(max_length=64, default=create_random_secret)
     description = models.TextField(blank=True,
                                    help_text='Description of this webhook, for staff-eyes only.')
 
