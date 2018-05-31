92ee66299996c635d95ef36ea7116460d92463cd
Sybren A. Stüvel <sybren@stuvel.eu>
2018-05-31 17:40:18 +0200

    Replaced 'date joined' with 'privacy policy agreed' in admin
    Also allow filtering on privacy_policy_agreed timestamp.

--- bid_main/admin.py
+++ bid_main/admin.py
@@ -129,11 +129,11 @@ class UserAdmin(BaseUserAdmin):
     )
 
     list_display = ('email', 'full_name', 'is_active', 'is_staff', 'role_names', 'last_update',
-                    'confirmed_email_at', 'date_joined')
+                    'confirmed_email_at', 'privacy_policy_agreed')
     list_display_links = ('email', 'full_name')
     list_filter = ('roles', 'is_active', 'groups',
                    'confirmed_email_at', 'is_staff', 'is_superuser',
-                   'date_joined')
+                   'date_joined', 'privacy_policy_agreed')
     list_per_page = 12
     search_fields = ('email', 'full_name', 'email_change_preconfirm', 'nickname')
     ordering = ('-last_update',)
