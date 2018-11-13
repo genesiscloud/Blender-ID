ab81111bf57529f4e9ac77c8f5f83214aeb2daf0
Sybren A. Stüvel <sybren@stuvel.eu>
2018-11-13 15:17:13 +0100

    Fixed Django deprecation warning

--- bid_main/tests/test_login_register.py
+++ bid_main/tests/test_login_register.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse, reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
