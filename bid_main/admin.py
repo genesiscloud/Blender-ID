cfeb297f6159c503ae047951c7216e49386a8840
Sybren A. Stüvel <sybren@stuvel.eu>
2019-01-04 12:54:56 +0100

    Admin: add nickname field to user creation form
    Without being able to set the nickname we can't create users.

--- bid_main/admin.py
+++ bid_main/admin.py
@@ -124,7 +124,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2', 'nickname'),
         }),
     )
 
